981fb05006d7cf664a5d5fc34d93fef9577c04ee
Tasneem <s-tasneem.attia@zewailcity.edu.eg>
2023-05-04 22:21:21 +0300

    Add Product Page

---
+++ SMARTLY/Pages/Product_internal.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SMARTLY.Pages
+{
+    public class Product_internalModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
