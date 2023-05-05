eafc8feb6ae7a9ccf161fe02854d4693e4807590
ZC <ZC@DESKTOP-A0CE1LT>
2023-05-06 01:52:16 +0300

    Admin Products Views in Main, Internal, Out

---
+++ SMARTLY/Pages/Bundle_Out_Admin.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SMARTLY.Pages
+{
+    public class Bundle_Out_AdminModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
