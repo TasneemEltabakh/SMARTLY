db41fe4fdca5901e17b7e8017eb96d2c63e6e613
Tasneem <s-tasneem.attia@zewailcity.edu.eg>
2023-05-06 01:44:30 +0300

    Contact

---
+++ SMARTLY/Pages/All_Agencies.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SMARTLY.Pages
+{
+    public class All_AgenciesModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
