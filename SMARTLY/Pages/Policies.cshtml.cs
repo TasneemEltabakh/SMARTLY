98beefd9f27912d4cee4d0737eba8b6eb7f33bf1
Tasneem <s-tasneem.attia@zewailcity.edu.eg>
2023-05-05 12:53:55 +0300

    product

---
+++ SMARTLY/Pages/Policies.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SMARTLY.Pages
+{
+    public class PoliciesModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
