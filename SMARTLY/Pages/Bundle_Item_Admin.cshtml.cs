e47a29248a7747e8ee47e9d886462a2045590959
ZC <ZC@DESKTOP-A0CE1LT>
2023-05-05 23:53:29 +0300

    Bundle_Admin

---
+++ SMARTLY/Pages/Bundle_Item_Admin.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SMARTLY.Pages
+{
+    public class Bundle_Item_AdminModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
