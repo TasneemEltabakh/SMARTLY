14f26f814dc7e24001633297495543666ede8eb9
nadasoudi <s-nada.soudi@zewailcity.edu.eg>
2023-05-05 20:55:46 +0300

    Bundle_Out

---
+++ SMARTLY/Pages/Bundle_Out.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SMARTLY.Pages
+{
+    public class Bundle_OutModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ SMARTLY/Pages/Shared/_Cart_Layout.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SMARTLY.Pages.Shared
+{
+    public class _Cart_LayoutModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
