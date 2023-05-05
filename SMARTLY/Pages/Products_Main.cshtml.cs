d8cabfdf5fafd4f1276601f22e0680ea942650a5
Tasneem <s-tasneem.attia@zewailcity.edu.eg>
2023-05-05 23:04:21 +0300

    Commit

--- SMARTLY/Pages/Products_Main.cshtml.cs
+++ SMARTLY/Pages/Products_Main.cshtml.cs
@@ -1,7 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace SMARTLY.Pages.ClientView
+namespace SMARTLY.Pages
 {
     public class Products_MainModel : PageModel
     {
