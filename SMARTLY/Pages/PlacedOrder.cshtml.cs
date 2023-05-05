09f69143dcd3f5f3582668ee6f6c336b7dc4b6f1
Tasneem <s-tasneem.attia@zewailcity.edu.eg>
2023-05-06 00:08:20 +0300

    Placed orders

---
+++ SMARTLY/Pages/PlacedOrder.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SMARTLY.Pages
+{
+    public class PlacedOrderModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
