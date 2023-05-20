aefd21e826de587339439068201b2eaf0637e8bb
ZC <ZC@DESKTOP-A0CE1LT>
2023-05-20 23:23:52 +0300

    Rghda Bundle views Client & Guest

--- SMARTLY/Pages/Bundle_ItemClient.cshtml.cs
+++ SMARTLY/Pages/Bundle_ItemClient.cshtml.cs
@@ -1,12 +1,36 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Data;
+using SMARTLY.Pages.Models;
 
 namespace SMARTLY.Pages
 {
     public class Bundle_ItemClientModel : UserPageModel
     {
-        public void OnGet()
-        {
+            private readonly Database data;
+            public DataTable dt { get; set; }
+            public DataTable dt22 { get; set; }
+            public int productnum { get; set; }
+
+            public int BundleId { get; set; }
+            public DataTable products { get; set; }
+            public Bundle_ItemClientModel(Database db)
+            {
+                data = db;
+            }
+            public void OnGet(int id)
+            {
+                dt = data.ReadBundleRow(id);
+                productnum = data.countProductsinBundle(id);
+                products = data.ProductsOfThisBundle(id);
+                dt22 = data.AllProduct();
+                BundleId = id;
+
+            }
+            public void OnPost(int Product_id)
+            {
+                data.AddProductToBundle(Product_id, BundleId);
+            }
         }
     }
-}
+
