944240ac4243e3f770b89771940ba6b1a29f9771
ZC <ZC@DESKTOP-A0CE1LT>
2023-06-20 19:53:40 +0300

    Bundle Out In Client only R

--- SMARTLY/Pages/Models/Database.cs
+++ SMARTLY/Pages/Models/Database.cs
@@ -24,7 +24,7 @@ namespace SMARTLY.Pages.Models
         public Object table { get; set; }
         public Database()
         {
-             Connection = new SqlConnection("Data Source=DESKTOP-1BNDCN7\\SQLEXPRESS;Initial Catalog=SMARTLY;Integrated Security=True");
+             Connection = new SqlConnection("Data Source=DESKTOP-A0CE1LT\\SQLEXPRESS;Initial Catalog=SMARTLY;Integrated Security=True");
 
         }
         public void SignUpNewMember(User U, Client C)
@@ -741,7 +741,7 @@ namespace SMARTLY.Pages.Models
 
         public DataTable ProductsOfThisBundle(int Id)   //***
         {
-            string Q = "select p.PName,p.Pimage from Bundle_Product BP, Product p where BP.product_id=p.PId and BP.Bundle_ID= " + @Id;
+            string Q = "select p.PName,p.Pimage,p.PId from Bundle_Product BP, Product p where BP.product_id=p.PId and BP.Bundle_ID= " + @Id;
             DataTable dt = new DataTable();
             try
             {
