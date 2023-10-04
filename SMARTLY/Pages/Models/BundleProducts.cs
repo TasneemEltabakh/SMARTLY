4b3d1d1f415814bbf286d477a3ddae5c9ed7f7df
Tasneem <s-tasneem.attia@zewailcity.edu.eg>
2023-10-04 20:26:31 +0300

    bundle images

---
+++ SMARTLY/Pages/Models/BundleProducts.cs
@@ -0,0 +1,13 @@
+﻿namespace SMARTLY.Pages.Models
+{
+	public class BundleProducts
+	{
+		public int Id { get; set; }
+		public int quantity { get; set; }
+		public BundleProducts(int id, int quantity)
+		{
+			Id = id;
+			this.quantity = quantity;
+		}	
+	}
+}
